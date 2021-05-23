82a736da0b8542786cfa9b83cccb625f09950edd
kanekodaichi <knkdic.bfc@gmail.com>
2021-05-24 00:38:41 +0900

    vue-cliでメモアプリを作成

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,124 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app" id="app">
+    <h1>Memo List</h1>
+    <ul>
+      <li v-for="(memo, index) in memos" v-bind:key="index">
+        <a v-on:click="edit(index)">{{ memo.split('\n')[0] }}</a>
+      </li>
+    </ul>
+    <div class="plus" v-on:click="show()">+</div>
+    
+    <div v-if="this.showPage">
+      <div class="show" v-if="!this.editIndex">
+        <h2>New Memo</h2>
+        <textarea v-model="newMemo"></textarea>
+        <button class="b-add" v-on:click="add()">追加</button>
+      </div>
+    </div>
+
+    <div class="show" v-if="this.editIndex">
+      <h2>Show Memo</h2>
+      <textarea v-model="editMemo"></textarea>
+      <button class="b-update" v-on:click="update(editIndex)">更新</button>
+      <button class="b-remove" v-on:click="remove(editIndex)">削除</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      memos: [],
+      newMemo: null,
+      editMemo: null,
+      editIndex: null,
+      showPage: false
+    };
+  },
+  mounted() {
+    if (localStorage.getItem("memos")) {
+      this.memos = JSON.parse(localStorage.memos);
+    }
+  },
+  methods: {
+    show: function() {
+      if (this.showPage === false) {
+        this.showPage = true;
+      } else {
+        this.showPage = false;
+      }
+    },
+    add: function() {
+      if (!this.newMemo) {
+        return;
+      }
+      this.memos.push(this.newMemo);
+      this.newMemo = "";
+      this.showPage = false;
+      this.save();
+    },
+    remove: function(index) {
+      this.memos.splice(index - 1, 1);
+      this.newMemo = "";
+      this.editIndex = null;
+      this.save();
+    },
+    edit: function(index) {
+      this.editMemo = this.memos[index];
+      this.editIndex = index + 1;
+    },
+    update: function(index) {
+      if (!this.editMemo) {
+        return;
+      }
+      this.memos[index - 1] = this.editMemo;
+      this.editMemo = "";
+      this.editIndex = null;
+      this.save();
+    },
+    save: function() {
+      localStorage.setItem("memos", JSON.stringify(this.memos));
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+textarea {
+  resize: none;
+  width: 200px;
+  height: 250px;
+  font-size: 1.5em;
 }
+
+.show {
+  position: relative;
+}
+
+.b-add {
+  position: absolute;
+  width: 100px;
+  bottom: -25px;
+  left: 50px;
+}
+
+.b-remove {
+  position: absolute;
+  width: 100px;
+  bottom: -25px;
+  left: 105px;
+}
+
+.b-update {
+  position: absolute;
+  width: 100px;
+  bottom: -25px;
+  left: 0px;
+}
+
+.plus {
+  font-size: 25px;
+}
+
 </style>
